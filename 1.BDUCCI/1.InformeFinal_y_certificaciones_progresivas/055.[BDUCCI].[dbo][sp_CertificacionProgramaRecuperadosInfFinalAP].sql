30f1c7e8fd33b083cd79b6367fd177d4f96b6d82
Emerson Herrera <emersonheto@gmail.com>
2025-08-21 16:23:16 -0500

    `feat`: datos certificacion recuperados - memorandum

--- 1.BDUCCI/1.InformeFinal_y_certificaciones_progresivas/055.[BDUCCI].[dbo][sp_CertificacionProgramaRecuperadosInfFinalAP].sql
+++ 1.BDUCCI/1.InformeFinal_y_certificaciones_progresivas/055.[BDUCCI].[dbo][sp_CertificacionProgramaRecuperadosInfFinalAP].sql
@@ -18,11 +18,13 @@ BEGIN
 
 	   INSERT INTO #RESULTADO
 	   SELECT  DISTINCT 
-			   Codigo ,
-			   Apellidos_Nombres
-		   FROM [dbo].[tblInfFinalMemorandumRecuperados]  --[dbo].[tblInfFinal_Memorandum_Recuperados] 
-		   WHERE Programa_Codigo=@ProgramCode 
-			 AND IdDocumentoFinalReportAp = @IdDocumentoFinalReportAp 
+			   A.Codigo ,
+			   A.Apellidos_Nombres
+		FROM [dbo].[tblInfFinalMemorandumRecuperados] AS A  --[dbo].[tblInfFinal_Memorandum_Recuperados] 
+		INNER JOIN [dbo].tblInfFinalCertificacionPrograma AS B ON A.IdDocumentoFinalReportAp = B.IdDocumentoFinalReportAp 
+		AND A.Codigo = B.Codigo		
+		WHERE A.Programa_Codigo=@ProgramCode 
+		AND A.IdDocumentoFinalReportAp = @IdDocumentoFinalReportAp
 			 
 		   --ORDER BY Apellidos_Nombres
 
